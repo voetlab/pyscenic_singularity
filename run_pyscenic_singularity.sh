82625e839a0ab8c37f9a7fcd6ba8307b56e2a0f0
Alejandro Sifrim <alejandro.sifrim@kuleuven.be>
2020-02-19 11:21:28 +0100

    Initial commit

---
+++ run_pyscenic_singularity.sh
@@ -0,0 +1,38 @@
+# Run pySCENIC using singularity
+# Requires a DATA FOLDER as an input that contains an exprMat.tsv file
+# This script currently processes mouse data
+# TODO: Make branch for human data
+
+SCENIC_IMAGE=/staging/leuven/stg_00002/voetlab/resources/singularity_images/aertslab-pyscenic-0.9.19.sif
+SCENIC_RESOURCES=/staging/leuven/stg_00002/voetlab/resources/SCENIC
+DATA_FOLDER=$1
+NR_WORKERS=20
+
+singularity exec -B $DATA_FOLDER:/scenicdata,$SCENIC_RESOURCES:/resources \
+$SCENIC_IMAGE \
+pyscenic grn \
+--num_workers $NR_WORKERS \
+-o /scenicdata/expr_mat.adjacencies.tsv \
+/scenicdata/exprMat.tsv \
+/resources/mm_tfs.txt
+
+
+singularity exec -B $DATA_FOLDER:/scenicdata,$SCENIC_RESOURCES:/resources \
+$SCENIC_IMAGE \
+pyscenic ctx \
+/scenicdata/expr_mat.adjacencies.tsv \
+/resources/mm9-500bp-upstream-7species.mc9nr.feather \
+/resources/mm9-tss-centered-10kb-7species.mc9nr.feather \
+--annotations_fname /resources/motifs-v9-nr.mgi-m0.001-o0.0.tbl \
+--expression_mtx_fname /scenicdata/exprMat.tsv \
+--mode "dask_multiprocessing" \
+--output /scenicdata/regulons.csv \
+--num_workers $NR_WORKERS
+
+singularity exec -B $DATA_FOLDER:/scenicdata,$SCENIC_RESOURCES:/resources \
+$SCENIC_IMAGE \
+pyscenic aucell \
+/scenicdata/exprMat.tsv \
+/scenicdata/regulons.csv \
+-o /scenicdata/auc_mtx.csv \
+--num_workers $NR_WORKERS
